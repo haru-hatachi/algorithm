263de4a8d25d9e026b27fec2052972d4cccd9629
haru hatchi <harukame100@gmail.com>
2025-01-05 18:12:27 +0900

    りすとの長さが9以外でもソートできるようにした。

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,10 +1,12 @@
-lista = [1,8,5,7,2,9,3,6,4]
-for i in range(10):
+lista = [4,8,5,7,2,9,3,6,1]
+rupukaisu = len(lista) - 1
+for i in range(rupukaisu):
     ban = 0
     print(lista)
-    for i in range(8):
+    for j in range(rupukaisu - i):
         if lista[ban] > lista[ban + 1]:
             memo = lista[ban + 1]
             lista[ban + 1] = lista[ban]
             lista[ban] = memo
         ban = ban + 1
+print(lista)
