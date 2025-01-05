fb17044dc80ae7299ff290582f32b8b51baac485
haru hatchi <harukame100@gmail.com>
2025-01-05 20:27:29 +0900

    関数化

--- prime-factorization.py
+++ prime-factorization.py
@@ -1,24 +1,29 @@
 import sys
-kazu = int(sys.argv[1])
-sosu = [2]
-soinsu = []
-ban = 0
-sosukamo = 3
-while kazu > 1:
-    while kazu % sosu[ban] == 0:
-        kazu = kazu / sosu[ban]
-        soinsu.append(sosu[ban])
-        print(sosu[ban])
-    memo = len(sosu)
-    while len(sosu) == memo:
-        ban = 0
-        kensa = 0
-        for i in range(len(sosu)):
-            if sosukamo % sosu[ban] == 0:
-                break
-            else:
-                kensa = kensa + 1
-            ban = ban + 1
-        if kensa == len(sosu):
-            sosu.append(sosukamo)
-        sosukamo = sosukamo + 1
+
+def primeFactorization(argv):
+    kazu = int(argv[1])
+    sosu = [2]
+    soinsu = []
+    ban = 0
+    sosukamo = 3
+    while kazu > 1:
+        while kazu % sosu[ban] == 0:
+            kazu = kazu / sosu[ban]
+            soinsu.append(sosu[ban])
+            print(sosu[ban])
+        memo = len(sosu)
+        while len(sosu) == memo:
+            ban = 0
+            kensa = 0
+            for i in range(len(sosu)):
+                if sosukamo % sosu[ban] == 0:
+                    break
+                else:
+                    kensa = kensa + 1
+                ban = ban + 1
+            if kensa == len(sosu):
+                sosu.append(sosukamo)
+            sosukamo = sosukamo + 1
+    return soinsu
+
+primeFactorization(sys.argv)
