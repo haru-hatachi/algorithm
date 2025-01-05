bf6c290226fa724c4b85db697b07189b3adf2a11
haru hatchi <harukame100@gmail.com>
2025-01-05 17:07:53 +0900

    変更前のリストを表示するようにした。

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,10 +1,10 @@
 lista = [1,8,5,7,2,9,3,6,4]
 for i in range(10):
     ban = 0
+    print(lista)
     for i in range(8):
         if lista[ban] > lista[ban + 1]:
             memo = lista[ban + 1]
             lista[ban + 1] = lista[ban]
             lista[ban] = memo
         ban = ban + 1
-    print(lista)
